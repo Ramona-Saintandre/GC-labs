af71a687c8cc0830df38fd3130ea236c25b3d59e
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-06 18:39:37 -0500

    declared the variables

---
+++ lab3-JS/main.js
@@ -0,0 +1,20 @@
+// Declare a variable with appropiate values
+// a variable is a container for a valuesa variable is ceclard using var,let,const
+
+let name= "Ramona Saintandre";
+let age= "49";
+let birthdate= "November 1, 1970"
+let gcGR= true;
+let lifeEvents= [
+    "I was born in Fulda, Germany.Fulda",
+    "I've been married for 15 years.",
+    "I have 8 grandkids.",
+    "I currently work at Vaneerden Food Service."
+];
+
+// If/Else statements (name, birthday, age)
+
+if gcGR {
+    
+}
+
