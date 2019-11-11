b25a4dfe05d63679d89e8b8816172052aaab4921
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-10 23:10:11 -0500

    created prompts but they are not working

---
+++ JSLab1-while-loop/main.js
@@ -0,0 +1,16 @@
+/**
+ * Ramona Saintandre 
+ * November 10th, 2019
+ * GC - JSLab1 - while loop game   
+ */
+
+ /**
+  * @let		string	userName  find out player1 name
+  */
+ let userName = prompt("Hello, What's your name");
+
+ /**
+  * @var		string	playGame  find out if the want to play
+  *//**
+   */
+ let playGame = prompt(`"Hello, ${userName} would you like to play a game?`);
