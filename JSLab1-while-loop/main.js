4c087b3a336c9cac31ec65436784fd1e4a39de25
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-10 23:28:42 -0500

    added docBlocks

--- JSLab1-while-loop/main.js
+++ JSLab1-while-loop/main.js
@@ -7,16 +7,17 @@
  /**
   * @let		string	userName  find out player1 name
   */
+
  let userName = prompt("Hello, What's your name");
 
  /**
-  * @var		string	playGame  find out if the want to play
+  * @let		string	playGame  find out if the want to play
   *//**
    */
  let playGame = prompt(`"Hello, ${userName} would you like to play a game?`);
  /**
-  * @if player says yes game starts
-  * @else player is prompted with a message 		string	playGame  find out if the want to play
+  * @if  playGame  player says yes game starts
+  * @else player   is prompted with a message 		string	playGame  find out if the want to play
   *//**
    */
  if (playGame () == "yes"){
@@ -24,4 +25,18 @@
     }else{
         prompt("Goodbye, CHICKEN!!!!")
     };
-    
+
+   
+
+    /**
+     * @let player1	integer	player1 starts with 40 points
+     */
+    player1 = 40;
+    /**
+     * @let	playergrant	number starts with 10 points 
+     */
+    playerGrant =10;
+    /**
+     * @let		number	player1wins when playerGrant reaches 0 three times
+     */
+        player1wins = 0;
