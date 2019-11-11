b37602ad92c45d97fd4a6aff7f2cda9a4c05de4e
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-11 01:05:26 -0500

    need to fix damage meter

--- JSLab1-while-loop/main.js
+++ JSLab1-while-loop/main.js
@@ -22,9 +22,9 @@
   *//**
    */
  if (playGame = "yes"){
-     alert(`Great, ${userName}, Let the games begin`) ;
+     alert(`Great, ${userName} Let the games begin`) ;
     }else{
-        alert("Goodbye, CHICKEN!!!!")
+        alert("Goodbye, CHICKEN!!!!");
     };
 
    
@@ -47,13 +47,15 @@
          * Start of game with a while loop 
          * @let	object	player1Health playerGrantHealth health/damage meter
          * not sure if I am using the right operator
+         * https://www.w3schools.com/js/js_comparisons.asp
+         * ===	equal value and equal type	x === 5	true
          */
-        while(player1Health >0 === playerGrantHealth>0){
+        while(player1Health >0 === playerGrantHealth> 0){
 
             /**
              * @console.log score with health that is left 		
              */
-            console.log(`${userName} has ${playerHealth} left`);
+            console.log(`${userName} has ${player1Health} left`);
             console.log(`Grant the Mighty Chicken has ${playerGrantHealth} left`);
         };
 
@@ -70,10 +72,24 @@
          * not sure if I should use ceil or floor , from reading the article I think I need floor for grant
          * because I am rounding down 
          * Ceil for player1 since they will get a point?
-         * 
+         * mulitplying by two because there are two numbers
          */
         player1Health = (Math.ceil(Math.random()*2));
         playerGrantHealth = (Math.floor(Math.random()*2));
+
+        
+
+        /**
+         * @let		mixed	damagePlayer1 
+         * Player1 does not reset, and wins the game after Grant has reset 3 times 
+         */
+        let damagePlayer1 = healthDamage - damageGrant;
+        /**
+         * @let	mixed	damageGrant Grant looses when he reaches 0 three times 
+         * So I need to configure this so that he loose a random point, and Player1 will get that point
+         * Grant's game resets when he reaches 0, after the third time Player1 is declared the winner
+         */
+        let damageGrant =   healthDamage - 
 
 
 
