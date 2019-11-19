bededfd70f86b775d594fd4c434902cbe816e403
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-18 19:57:36 -0500

    more docblock clean up

--- JSLab1-while-loop/main.js
+++ JSLab1-while-loop/main.js
@@ -10,8 +10,12 @@
  * @else player   is prompted with a message 		string	playGame  find out if the want to play
  * good bye alert is not working 
  */
-let userName = window.prompt ("Hello, What's your name");
- let playGame = window.prompt (`"Hello, ${userName} would you like to play a game?`);
+player1Health = 40;
+playerGrantHealth = 10;
+player1wins = 0;
+
+let userName = prompt ("Hello, What's your name");
+let playGame = prompt (`"Hello, ${userName} would you like to play a game?`);
 
 if (playGame = "yes") {
   alert(`Great, ${userName} Let the games begin`);
@@ -19,21 +23,14 @@ if (playGame = "yes") {
   alert("Goodbye, CHICKEN!!!!");
 };
 
-
-
 /**
  * how does the game start ?
  * @let player1Health	number	player1 starts with 40 points
- */
-player1Health = 40;
-/**
  * @let	playergrantHealth	number starts with 10 points 
- */
-playerGrantHealth = 10;
-/**
  * @let	player1wins 	number	player1wins when playerGrant reaches 0 three times
  */
-player1wins = 0;
+
+
 
 /**
  * Start of game with a while loop 
@@ -44,22 +41,20 @@ player1wins = 0;
  * &&	and	(x < 10 && y > 1) is true
  */
 while (player1Health > 0 === player1wins < 3) {
-
-  /**
-   * @console.log score with health that is left 		
-   */
+  
   console.log(`${userName} has ${player1Health} left`);
   console.log(`Grant the Mighty Chicken has ${playerGrantHealth} left`);
 };
 
 /**
- * @let		array	healthDamage
+ * @let	array	healthDamage
  * loop will remove random number either 1 or 2 from health for damage 
  */
+
 let healthDamage = [1, 2];
 
 /**
- * @var	player1Health	mixed	
+ * @let	player1Health	mixed	
  * https://www.w3schools.com/jsref/jsref_ceil.asp 
  * https://www.w3schools.com/jsref/jsref_floor.asp 
  * not sure if I should use ceil or floor , from reading the article I think I need floor for grant
@@ -70,29 +65,23 @@ let healthDamage = [1, 2];
 player1Health = (Math.ceil - (Math.random() * 2));
 playerGrantHealth = (Math.ceil - (Math.random() * 2));
 
-
-
 /**
  * @let		mixed	damagePlayer1 
  * Player1 does not reset, and wins the game after Grant has reset 3 times 
- */
-
-/**
  * @let	mixed	damageGrant Grant looses when he reaches 0 three times 
  * So I need to configure this so that he loose a random point, and Player1 will get that point
  * Grant's game resets when he reaches 0, after the third time Player1 is declared the winner
  */
-//  let damageGrant =   healthDamage - I am totally lost. I can not figure out how to bring this all together 
-// This is how far I was able to get
-// Sorry
 
 if (playerGrantHealth <= 0); {
   player1wins++;
   playerGrantHealth = 10; //reseting Grant
-};
+};// end of while loop
+
+//winner declaration
 
 if (player1wins = 3) {
-  console.log('Player1 is totally awesome!!!')
+  console.log('Player1 is totally awesome!!!');
 } else {
   console.log('Grant has kicked some butt');
 };
