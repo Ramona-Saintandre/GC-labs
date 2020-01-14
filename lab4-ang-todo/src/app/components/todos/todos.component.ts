64d797991e1dbff995bfab431555a511f1d9d019
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-14 00:22:35 -0500

    added 3 more items to the array, forgot brackets

--- lab4-ang-todo/src/app/components/todos/todos.component.ts
+++ lab4-ang-todo/src/app/components/todos/todos.component.ts
@@ -12,11 +12,28 @@ export class TodosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.todos ={
+    this.todos = [
+      {
       id: 1,
       title:'Todo One',
       completed:false
+    },
+    {
+      id: 2,
+      title:'Todo One',
+      completed:false
+    },
+    {
+      id: 3,
+      title:'Todo One',
+      completed:false
+    },
+    {
+      id: 4,
+      title:'Todo One',
+      completed:false
     }
+  ]
   }
-
 }
+
