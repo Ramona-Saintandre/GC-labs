95bbcaeb45fca275bdceb1414ea472356cfdceaf
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-19 21:55:54 -0500

    removed HTTPclient for now

--- lab4-ang-todo/src/app/app.module.ts
+++ lab4-ang-todo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from './app.component';
+// import { HttpClientModule } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
@@ -17,7 +17,7 @@ import { TodoItemsComponent } from './components/todo-items/todo-items.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
