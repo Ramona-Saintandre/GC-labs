515c6ed8c11b006a0acd102c9efd3bcdf9d0e30f
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-23 18:50:43 -0500

    set up filter pipe but it is not working

--- lab4/lab4-ang-todolist/src/app/filter.pipe.ts
+++ lab4/lab4-ang-todolist/src/app/filter.pipe.ts
@@ -10,3 +10,25 @@ export class FilterPipe implements PipeTransform {
   }
 
 }
+
+// import { Pipe, PipeTransform } from '@angular/core';
+// import { Todo } from './models/Todo';
+
+// @Pipe({
+//   name: 'filter'
+// })
+// export class FilterPipe implements PipeTransform {
+//   transform(items: Todo[], searchText: string) : Todo[] {
+//     if (!items) return [];
+
+//     if (!searchText) return items;
+
+//     searchText = searchText.toLowerCase();
+
+//     return items.filter(it => {
+//       return it.title.toLowerCase().includes(searchText);
+//     });
+//   }
+//   filter: string = '';
+
+// }
