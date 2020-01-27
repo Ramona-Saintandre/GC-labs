6a5f49bf7a7f2467151a5fe473b249f38f857284
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-26 22:13:36 -0500

    created social post and post component

--- lab5-socialpost/src/app/app.module.ts
+++ lab5-socialpost/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialpostComponent } from './socialpost/socialpost.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialpostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
